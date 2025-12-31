addb8c2315e31307ea40f8db2633f9046a8a6ade
Ehsan <dev2024loper@gmail.com>
2025-12-31 20:47:03 +0330

    uncommented

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,20 +1,21 @@
 import unittest
 from math_utils import multiply, is_even
 
-# class testMultiply(unittest.TestCase):
-#     def test_multiply_two_numbers(self):
-#         self.assertEqual(multiply(2, 4), 8)
+class testMultiply(unittest.TestCase):
+    def test_multiply_two_numbers(self):
+        self.assertEqual(multiply(2, 4), 8)
 
-#     def test_multiply_with_none(self):
-#         with self.assertRaises(ValueError):
-#             multiply(None, 3)
+    def test_multiply_with_none(self):
+        with self.assertRaises(ValueError):
+            multiply(None, 3)
+
 
 class testIsEven(unittest.TestCase):
-    # def test_is_even(self):
-    #     self.assertTrue(is_even(4))
+    def test_is_even(self):
+        self.assertTrue(is_even(4))
 
-    # def test_is_odd(self):   
-    #     self.assertFalse(is_even(5))
+    def test_is_odd(self):   
+        self.assertFalse(is_even(5))
 
     def test_invalid_inputs(self):
         invalid_inputs = ["4", None, 2.2]
